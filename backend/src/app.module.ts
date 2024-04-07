a24ea2add7842183f29a3a7bf67fe1353e767ff1
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2024-04-07 00:13:18 -0300

    Correção do acesso a variável de ambiente

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ChatModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -5,7 +5,6 @@ const APIKEY = process.env.GEMINI_API_KEY;
 @Injectable()
 export class ChatService {
   async generateContentAPI() {
-    console.log(APIKEY);
     const genAI = new GoogleGenerativeAI(APIKEY);
     const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
     const prompt = 'Write a story about a magic backpack.';
