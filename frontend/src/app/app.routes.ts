e674ba9812d2b183d0d10b5752ce8884e9131441
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2024-04-19 23:50:14 -0300

    Criação da página de prompt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PromptComponent } from './prompt/prompt.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'prompt' },
+  {
+    path: 'prompt',
+    component: PromptComponent,
+  },
+  { path: '**', redirectTo: '/prompt' },
+];
---
+++ frontend/src/app/prompt/prompt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prompt',
+  standalone: true,
+  imports: [],
+  templateUrl: './prompt.component.html',
+  styleUrl: './prompt.component.css'
+})
+export class PromptComponent {
+
+}
