94bb9a4625ecc8bd149ad1ec8a0e61ff2fe2bedc
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2024-04-20 01:36:40 -0300

    Correção de CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Gemini Backend')
