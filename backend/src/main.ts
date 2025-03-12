0dc72887c3967bb769a183be55ad75c5dbc157f9
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2025-03-11 23:52:23 -0300

    Correcao de environment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 async function bootstrap() {
+
+  dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
--- backend/src/prompt/prompt.service.ts
+++ backend/src/prompt/prompt.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { GenAiResponse } from './response.interface';
 import { Content, GoogleGenerativeAI } from '@google/generative-ai';
 
-const APIKEY = process.env.GEMINI_API_KEY;
-
 @Injectable()
 export class PromptService {
   createContent(text: string): Content[] {
@@ -20,10 +18,10 @@ export class PromptService {
   }
 
   async generateText(prompt: string): Promise<GenAiResponse> {
+    const APIKEY = process.env.GEMINI_API_KEY;
     const contents = this.createContent(prompt);
     const genAI = new GoogleGenerativeAI(APIKEY);
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
-
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
     const result = await model.generateContent({ contents });
     const response = await result.response;
     const text = response.text();
